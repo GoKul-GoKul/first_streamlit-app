172a1580ab392cda856b6412d5c3cb01cc4bb582
GoKul-GoKul <85825587+GoKul-GoKul@users.noreply.github.com>
2023-06-23 22:37:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-write your own comment - what does this do?
+# write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
