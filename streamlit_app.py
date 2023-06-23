6a2278d9bb52b1b3814e986585618c386e1b4c1c
GoKul-GoKul <85825587+GoKul-GoKul@users.noreply.github.com>
2023-06-23 22:23:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = myfruits_list.loc[fruits_selected]
 #streamlit dataframe
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
 #requests
