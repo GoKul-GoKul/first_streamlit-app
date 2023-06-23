b2d0fffddd69b6b5695280470b5b2843012bf248
GoKul-GoKul <85825587+GoKul-GoKul@users.noreply.github.com>
2023-06-23 22:11:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,10 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+#add a text box challenge lab
+
+# streamlit.header("What Fruit would you like to have?")
+add_my_fruit = streamlit.text_input = ("What Fruit would you like to have?", "Jackfruit")
+streamlit.text("Thanks for adding", add_my_fruit)
