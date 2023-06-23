fdad9b3aa3ff0a075f0a5ae2adf794194aa8dab6
GoKul-GoKul <85825587+GoKul-GoKul@users.noreply.github.com>
2023-06-23 22:44:55 +0530

    Update streamlit_app.py New Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,14 +23,27 @@ fruits_to_show = myfruits_list.loc[fruits_selected]
 #streamlit dataframe
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
 #requests
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+#add a text box challenge lab
+streamlit.header("What Fruit would you like to have?")
+add_my_fruit = streamlit.text_input('What Fruit would you like to have?', 'Jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
